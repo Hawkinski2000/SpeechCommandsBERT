467bee51fab67128f4840e9a2c60478d4660a07c
Kieran <hawkinski2019@gmail.com>
2025-04-07 00:13:09 -0700

    feat(data): implement loading val and test splits
    Implement loading and processing validation and test splits and saving
    their spectrograms and labels to the data directory.

--- data.py
+++ data.py
@@ -1,7 +1,3 @@
-""""
-TODO:
- - Implement test/val sets.
-"""
 from datasets import load_dataset
 import os
 import numpy as np
@@ -15,17 +11,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 # ----------------------------------------------------------------------------
-# ---- Load Dataset and Create "data" Directory for Audio and Labels ----
-
-# https://huggingface.co/datasets/google/speech_commands
-ds = load_dataset("google/speech_commands", 
-                  "v0.02",
-                  split="train",
-                  trust_remote_code=True)
-ds = ds.shuffle(seed=1337)
-
-# Use a total of 10 shards each for spectrograms and their labels
-shard_size = int(np.ceil(len(ds) / 10))
+# ---- Create "data" Directory for Audio and Labels ----
 
 # Create "data" directory to hold "audio" and "labels"
 data_dir = os.path.join(os.path.dirname(__file__), "data")
@@ -38,10 +24,24 @@ labels_dir = os.path.join(data_dir, "labels")
 os.makedirs(audio_dir, exist_ok=True)
 os.makedirs(labels_dir, exist_ok=True)
 
-print(f"Number of examples for each class: {list(Counter(ds["label"]).values())}")
-
 # ----------------------------------------------------------------------------
-# ---- Prepare Spectrograms and Labels ----
+# ---- Functions for Loading Split and Preparing Spectrograms and Labels ----
+
+
+def load_split(split):
+    # https://huggingface.co/datasets/google/speech_commands
+    ds = load_dataset("google/speech_commands", 
+                    "v0.02",
+                    split=split,
+                    trust_remote_code=True)
+    ds = ds.shuffle(seed=1337)
+
+    # Use a total of 10 shards each for spectrograms and their labels
+    shard_size = int(np.ceil(len(ds) / 10))
+
+    print(f"Number of examples for each class in {split} split: {list(Counter(ds["label"]).values())}")
+
+    return ds, shard_size
 
 
 def tokenize(sample):
@@ -94,55 +94,57 @@ def tokenize(sample):
 
 
 def main():
-    shard_index = 0
-    example_count = 0
-    progress_bar = None
-    spectrograms = []
-    labels = []
-    position = 0
-    total = len(ds)
+    for split in ["train", "validation", "test"]:
+        ds, shard_size = load_split(split)
+        shard_index = 0
+        example_count = 0
+        progress_bar = None
+        spectrograms = []
+        labels = []
+        position = 0
+        total = len(ds)
 
-    for sample in ds:
-        position += 1
-        
-        log_mel_spectrogram, label = tokenize(sample)
-        
-        if log_mel_spectrogram == None:
-            continue
-
-        # Create a progress bar for this pair of shards
-        if progress_bar is None:
-            progress_bar = tqdm(total=shard_size,
-                                unit=" Examples",
-                                desc=f"Shard {shard_index}")
+        for sample in ds:
+            position += 1
             
-        spectrograms.append(log_mel_spectrogram)
-        labels.append(label)
-        example_count += 1
+            log_mel_spectrogram, label = tokenize(sample)
+            
+            if log_mel_spectrogram == None:
+                continue
 
-        # update progress bar
-        progress_bar.update(1)
+            # Create a progress bar for this pair of shards
+            if progress_bar is None:
+                progress_bar = tqdm(total=shard_size,
+                                    unit=" Examples",
+                                    desc=f"Shard {shard_index}")
+                
+            spectrograms.append(log_mel_spectrogram)
+            labels.append(label)
+            example_count += 1
 
-        # Save the pair of shards if they have been filled
-        if example_count == shard_size or position == total:
+            # update progress bar
+            progress_bar.update(1)
 
-            # Save spectrograms shard to "audio" directory
-            audio_tensor = torch.cat(spectrograms)
-            audio_path = os.path.join(
-                audio_dir, f"train_audio_{shard_index:02d}.pt")
-            torch.save(audio_tensor.cpu(), audio_path)
+            # Save the pair of shards if they have been filled
+            if example_count == shard_size or position == total:
 
-            # Save labels shard to "labels" directory
-            labels_tensor = torch.tensor(labels)
-            labels_path = os.path.join(
-                labels_dir, f"train_labels_{shard_index:02d}.pt")
-            torch.save(labels_tensor.cpu(), labels_path)
+                # Save spectrograms shard to "audio" directory
+                audio_tensor = torch.cat(spectrograms)
+                audio_path = os.path.join(
+                    audio_dir, f"{split}_audio_{shard_index:02d}.pt")
+                torch.save(audio_tensor.cpu(), audio_path)
 
-            shard_index += 1
-            example_count = 0
-            progress_bar = None
-            spectrograms = []
-            labels = []
+                # Save labels shard to "labels" directory
+                labels_tensor = torch.tensor(labels)
+                labels_path = os.path.join(
+                    labels_dir, f"{split}_labels_{shard_index:02d}.pt")
+                torch.save(labels_tensor.cpu(), labels_path)
+
+                shard_index += 1
+                example_count = 0
+                progress_bar = None
+                spectrograms = []
+                labels = []
 
 if __name__ == '__main__':
     main()
